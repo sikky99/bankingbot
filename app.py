b013d88261074cd276de800bf470f9b62b32948c
sikky99 <swapnil.vishwakarma101@gmail.com>
2018-08-04 12:44:27 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template, request
+from chatterbot.trainers import ListTrainer
+from chatterbot import ChatBot
+from chatterbot.trainers import ChatterBotCorpusTrainer
+import os
+
+app = Flask(__name__)
+
+english_bot = ChatBot("Chatterbot", storage_adapter="chatterbot.storage.SQLStorageAdapter")
+
+
+
+
+english_bot.set_trainer(ListTrainer)
+
+
+for _file in os.listdir('files'):
+	chats = open('files/' + _file, 'r').readlines()
+
+	english_bot.train(chats)
+
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/get")
+def get_bot_response():
+    userText = request.args.get('msg')
+    return str(english_bot.get_response(userText))
+
+
+if __name__ == "__main__":
+    app.run()
